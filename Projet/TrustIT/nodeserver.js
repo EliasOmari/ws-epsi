4a6248995aa4071957453d35947b85f52ccfc811
Alexandre Boure <alexandre@boure.org>
2021-09-28 15:54:53 +0200

    Update nodeserver to read html files

--- Projet/TrustIT/nodeserver.js
+++ Projet/TrustIT/nodeserver.js
@@ -1,12 +1,22 @@
 const http = require('http')
+const url = require('url')
+const fs = require('fs')
 
 const hostname = '127.0.0.1'
 const port = 3000
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World\n')
+const server = http.createServer((request, response) => {
+    fs.readFile(__dirname + request.url, function(error, data) {
+        if (error) {
+            response.writeHead(404)
+            response.end()
+        }
+        else {
+            response.writeHead(200, {"Content-Type": "text/html"})
+            response.write(data, "utf8")
+            response.end()
+        }
+    })
 })
 
 server.listen(port, hostname, () => {
