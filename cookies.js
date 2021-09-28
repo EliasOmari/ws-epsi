09589743239fdfb9d8d3e38385eb49926373fe8f
Salah-Eddine ET-TALEBY <salah.ettaleby7@gmail.com>
2021-09-28 15:21:22 +0000

    Cookies

---
+++ cookies.js
@@ -0,0 +1,30 @@
+const puppeteer = require("puppeteer");
+
+const URL = "https://serverfault.com/";
+async function main() {
+  const browser = await puppeteer.launch({
+    ignoreDefaultArgs: ["--disable-extensions"],
+    args: [
+      "--disable-gpu",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--no-first-run",
+      "--no-sandbox",
+      "--no-zygote",
+      "--single-process"
+    ]
+  });
+  const page = await browser.newPage();
+
+  await page.goto(URL);
+
+  var data = await page._client.send("Network.getAllCookies");
+
+  // Pour récupérer le nom du premier cookie
+  console.log(data.cookies[0].name);
+
+  data.cookies.forEach((cookie) => {
+      console.log("nom : ", cookie.secure);
+  })
+}
+main();
