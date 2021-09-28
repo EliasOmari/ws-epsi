16699d5953b65cfc6279656b5168c2c145f205e5
Elias <eliasomari.eo@gmail.com>
2021-09-28 11:08:18 +0200

    ajout du front

--- Projet/TrustIT/src/app/app.module.ts
+++ Projet/TrustIT/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    HeaderComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
