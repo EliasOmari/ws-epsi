f7cecf9f0ad6b95096a5117a497683361f0e971a
Alexandre Boure <alexandre@boure.org>
2021-09-28 16:09:07 +0200

    Handle both GET and POST methods in node server

--- Projet/TrustIT/nodeserver.js
+++ Projet/TrustIT/nodeserver.js
@@ -1,22 +1,39 @@
 const http = require('http')
 const url = require('url')
 const fs = require('fs')
+const querystring = require('querystring')
 
 const hostname = '127.0.0.1'
 const port = 3000
 
 const server = http.createServer((request, response) => {
-    fs.readFile(__dirname + request.url, function(error, data) {
-        if (error) {
-            response.writeHead(404)
-            response.end()
-        }
-        else {
-            response.writeHead(200, {"Content-Type": "text/html"})
-            response.write(data, "utf8")
-            response.end()
-        }
-    })
+    if (request.method == 'GET') {
+        fs.readFile(__dirname + request.url, (error, data) => {
+            if (error) {
+                response.writeHead(404)
+                response.end()
+            }
+            else {
+                response.writeHead(200, {"Content-Type": "text/html"})
+                response.write(data, "utf8")
+                response.end()
+            }
+        })
+    else if (request.method == 'POST') {
+        var body = ''
+
+        request.on('data', (data) => {
+            body += data
+
+        if (body.length > 1e6)
+            request.connection.destroy()
+        })
+
+        request.on('end', () => {
+            var post = querystring.parse(body)
+            console.log(post)
+        })
+    }
 })
 
 server.listen(port, hostname, () => {
