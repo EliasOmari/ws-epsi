6b2c354295cfbe1ca494fc95fd5ad94ab77578b4
Alexandre Boure <alexandre@boure.org>
2021-09-29 12:00:08 +0200

    Send example json

--- Projet/TrustIT/nodeserver.js
+++ Projet/TrustIT/nodeserver.js
@@ -30,7 +30,74 @@ const server = http.createServer((request, response) => {
 
         request.on('end', () => {
             var post = querystring.parse(body)
-            console.log(post)
+            console.log(post.oSaisie)
+
+            response.writeHead(200, {"Content-Type": "application/json"})
+            response.write(`{
+  "cookies": [
+    {
+      "name": "prov",
+      "value": "1ea3ed7d-1604-4f16-69e4-f13235103476",
+      "domain": ".serverfault.com",
+      "path": "/",
+      "expires": 2682374401.645036,
+      "size": 40,
+      "httpOnly": true,
+      "secure": false,
+      "session": false,
+      "priority": "Medium",
+      "sameParty": false,
+      "sourceScheme": "Secure",
+      "sourcePort": 443
+    },
+    {
+      "name": "_ga",
+      "value": "GA1.2.218586659.1632907209",
+      "domain": ".serverfault.com",
+      "path": "/",
+      "expires": 1695979209,
+      "size": 29,
+      "httpOnly": false,
+      "secure": false,
+      "session": false,
+      "priority": "Medium",
+      "sameParty": false,
+      "sourceScheme": "Secure",
+      "sourcePort": 443
+    },
+    {
+      "name": "_gid",
+      "value": "GA1.2.948062075.1632907209",
+      "domain": ".serverfault.com",
+      "path": "/",
+      "expires": 1632993609,
+      "size": 30,
+      "httpOnly": false,
+      "secure": false,
+      "session": false,
+      "priority": "Medium",
+      "sameParty": false,
+      "sourceScheme": "Secure",
+      "sourcePort": 443
+    },
+    {
+      "name": "_gat",
+      "value": "1",
+      "domain": ".serverfault.com",
+      "path": "/",
+      "expires": 1632907269,
+      "size": 5,
+      "httpOnly": false,
+      "secure": false,
+      "session": false,
+      "priority": "Medium",
+      "sameParty": false,
+      "sourceScheme": "Secure",
+      "sourcePort": 443
+    }
+  ]
+}`, "utf8")
+            response.end()
         })
     }
 })
