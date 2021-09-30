c706406f0f1ff760dc0b1dc497fed8dc620f7820
Alexandre Boure <alexandre@boure.org>
2021-09-30 09:51:13 +0200

    Add puppeteer and fix GET and POST request targets

--- Projet/TrustIT/nodeserver.js
+++ Projet/TrustIT/nodeserver.js
@@ -1,13 +1,20 @@
-const http = require('http')
-const url = require('url')
 const fs = require('fs')
+const http = require('http')
+const puppeteer = require("puppeteer")
 const querystring = require('querystring')
+const url = require('url')
 
 const hostname = '127.0.0.1'
 const port = 3000
 
 const server = http.createServer((request, response) => {
+    console.log(request.url)
+
     if (request.method == 'GET') {
+        if (request.url == '/' ) {
+            request.url = '/index.html'
+        }
+
         fs.readFile(__dirname + request.url, (error, data) => {
             if (error) {
                 response.writeHead(404)
@@ -18,7 +25,7 @@ const server = http.createServer((request, response) => {
                 response.end()
             }
         })
-    } else if (request.method == 'POST') {
+    } else if (request.method == 'POST' && request.url == '/recherche/') {
         var body = ''
 
         request.on('data', (data) => {
@@ -30,73 +37,36 @@ const server = http.createServer((request, response) => {
 
         request.on('end', () => {
             var post = querystring.parse(body)
-            console.log(post.oSaisie)
+            var url = post.oSaisie
+
+        async function puppet() {
+            const browser = await puppeteer.launch({
+                ignoreDefaultArgs: ["--disable-extensions"],
+                args: [
+                    "--disable-gpu",
+                    "--disable-dev-shm-usage",
+                    "--disable-setuid-sandbox",
+                    "--no-first-run",
+                    "--no-sandbox",
+                    "--no-zygote",
+                    "--single-process"
+                ]
+            })
+
+            const page = await browser.newPage()
+            // const response = await page.goto(url)
+            // const securityDetails = response.securityDetails()
+            // const validFrom = securityDetails.validFrom() * 1000
+            // const expiryDate = securityDetails.validTo() * 1000
+
+            // console.log(new Date(validFrom));
+            // console.log(new Date(expiryDate));
+            // console.log(securityDetails.issuer());
+        }
+        puppet()
 
             response.writeHead(200, {"Content-Type": "application/json"})
-            response.write(`{
-  "cookies": [
-    {
-      "name": "prov",
-      "value": "1ea3ed7d-1604-4f16-69e4-f13235103476",
-      "domain": ".serverfault.com",
-      "path": "/",
-      "expires": 2682374401.645036,
-      "size": 40,
-      "httpOnly": true,
-      "secure": false,
-      "session": false,
-      "priority": "Medium",
-      "sameParty": false,
-      "sourceScheme": "Secure",
-      "sourcePort": 443
-    },
-    {
-      "name": "_ga",
-      "value": "GA1.2.218586659.1632907209",
-      "domain": ".serverfault.com",
-      "path": "/",
-      "expires": 1695979209,
-      "size": 29,
-      "httpOnly": false,
-      "secure": false,
-      "session": false,
-      "priority": "Medium",
-      "sameParty": false,
-      "sourceScheme": "Secure",
-      "sourcePort": 443
-    },
-    {
-      "name": "_gid",
-      "value": "GA1.2.948062075.1632907209",
-      "domain": ".serverfault.com",
-      "path": "/",
-      "expires": 1632993609,
-      "size": 30,
-      "httpOnly": false,
-      "secure": false,
-      "session": false,
-      "priority": "Medium",
-      "sameParty": false,
-      "sourceScheme": "Secure",
-      "sourcePort": 443
-    },
-    {
-      "name": "_gat",
-      "value": "1",
-      "domain": ".serverfault.com",
-      "path": "/",
-      "expires": 1632907269,
-      "size": 5,
-      "httpOnly": false,
-      "secure": false,
-      "session": false,
-      "priority": "Medium",
-      "sameParty": false,
-      "sourceScheme": "Secure",
-      "sourcePort": 443
-    }
-  ]
-}`, "utf8")
+            response.write("{}", "utf8")
             response.end()
         })
     }
